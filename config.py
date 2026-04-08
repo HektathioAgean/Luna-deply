41e6485d2506028a85282990fc34003cd3bd2499
HektathioAgean <HektathioAgean@proton.me>
2026-04-08 16:53:43 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,18 +6,6 @@ ASSETS_DIR = BASE_DIR / "assets"
 EXPORT_DIR = BASE_DIR / "exports"
 DATA_DIR = BASE_DIR / "data"
 
-
-def initialize_directories() -> None:
-    """
-    Cria os diretórios necessários para o projeto.
-    Deve ser chamada explicitamente no início da aplicação.
-    """
-    SRC_DIR.mkdir(parents=True, exist_ok=True)
-    ASSETS_DIR.mkdir(parents=True, exist_ok=True)
-    EXPORT_DIR.mkdir(parents=True, exist_ok=True)
-    DATA_DIR.mkdir(parents=True, exist_ok=True)
-
-
 APP_NAME = "Luna"
 APP_TITLE = "Luna | Análise de Tempos"
 LAYOUT = "wide"
@@ -36,3 +24,16 @@ AVAILABLE_UNITS = [
     "PG",
     "NP",
 ]
+
+
+def initialize_directories() -> None:
+    """
+    Cria os diretórios necessários para o projeto.
+    """
+    SRC_DIR.mkdir(parents=True, exist_ok=True)
+    ASSETS_DIR.mkdir(parents=True, exist_ok=True)
+    EXPORT_DIR.mkdir(parents=True, exist_ok=True)
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+
+initialize_directories()
